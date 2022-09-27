405c7dd334ff58b2ac1630e14be25b76cbf95825
chethuuu <chethanaprasadi2000@gmail.com>
2022-09-27 19:59:46 +0530

    Add User Side Interface

---
+++ client/src/Components/Room/Customer/CusRoom.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import SimpleImageSlider from "react-simple-image-slider";
+import CusAllRooms from './CusAllRoom';
+import '../Rooms.css'
+
+const images = [
+    { url: "https://www.wijayabeach.com/conmain/images/IMG_0151.jpg" },
+    { url: "https://www.wijayabeach.com/conmain/images/IMG_9471.jpg" },
+    { url: "http://wallpapers.net/maldives-hotel-room-hd-wallpaper/download/828x350.jpg" },
+    { url: "https://www.telegraph.co.uk/content/dam/Travel/hotels/asia/sri-lanka/kk-beach-sri-lanka-l-xlarge.jpg?imwidth=1280" },
+
+];
+
+const CusRoom = () => (
+
+    <blockquote class="blockquote text-center">
+        <div>
+            <SimpleImageSlider
+                width={1540}
+                height={400}
+                images={images}
+                showBullets={true}
+                showNavs={true}
+            />
+        </div>
+
+        <CusAllRooms/>
+        
+    </blockquote>
+    
+
+)
+
+export default CusRoom;
