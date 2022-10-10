a382cddd71961be771b583215b28f11e83abb8c0
minosh00 <it20088682@my.sliit.lk>
2022-10-10 13:13:44 +0530

    push

--- client/src/Components/Restaurants/Client/AddTocart.js
+++ client/src/Components/Restaurants/Client/AddTocart.js
@@ -1,25 +1,70 @@
-import React , {useEffect} from 'react'
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 
 const AddToCart = (props) => {
- 
-  useEffect(() => {
-    console.log("data",props?.data);
-  }, []);
+  const Quantity = 1;
 
+  useEffect(() => {
+    console.log("data", props?.data);
+  }, []);
 
   return (
     <div>
-      <div>add to cart</div>
-      <div>
-        {props?.data?.map((item)=>{
-          return (
-            <div>
-              {item?.foodName} 
-              {item?.price}   
-            </div>
-          )
-        })}
-      </div>
+      <Link to="/Displaymenus"><span type="submit" class="badge rounded-pill badge-warning">back to menu list </span></Link>
+      <MDBTable align='middle' >
+        <MDBTableHead>
+          <tr>
+            <th scope='col'>Order Menu Images   </th>
+            <th scope='col'>Order Menu  item Description </th>
+            <th scope='col'>Order Menu  item Quantity  </th>
+            <th scope='col'>Order Menu  item   price </th>
+            <th scope='col'>Each Item Price </th>
+          </tr>
+        </MDBTableHead>
+        <MDBTableBody  >
+
+          {props?.data?.map((item) => {
+            return (
+              <tr>
+                <td>
+                  <div className='d-flex align-items-center'>
+                    <img
+                      src={item?.images}
+                      alt=''
+                      style={{ width: '45px', height: '45px' }}
+                      className='rounded-circle'
+                    />
+                    <div className='ms-3'>
+                      <p className='fw-bold mb-1'>  {item?.foodName}</p>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <p className='fw-normal mb-1'>    {item?.Description}</p>
+                </td>
+                <td>
+                  <p className='fw-normal mb-1'>    {Quantity}</p>
+                </td>
+
+                <td>
+                  <h4> <MDBBadge color='success' pill>
+                    {item?.price}
+                  </MDBBadge></h4>
+                </td>
+         
+              </tr>
+    
+            )
+          
+
+          })}
+
+        </MDBTableBody>
+
+      
+      </MDBTable>
+
     </div>
   )
 }
--- client/src/Components/Restaurants/Client/Displaymenus.js
+++ client/src/Components/Restaurants/Client/Displaymenus.js
@@ -1,45 +1,51 @@
-import React from 'react'
+import styles from "./Cart.css";
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import {
-    Badge,
-    Card,
-    CardHeader,
-    CardTitle,
-    CardBody,
-    Label,
-    Button,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    Input,
-    Form
+  Badge,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Label,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Input,
+  Form
 } from "reactstrap";
 import AddToCart from "./AddTocart";
 
+
+
+
+
 const Displaymenus = () => {
 
-    
-  const [serachItem,setserachItem] =useState([]);
+  const [number, setNumber] = useState(1);
+
+
+  const [serachItem, setserachItem] = useState([]);
   const [users, setusers] = useState();
   const [loading, setloading] = useState(true);
   const [cartData, setcartData] = useState([]);
   const [ShowCart, setShowCart] = useState(false);
 
-   var array = [];
+  var array = [];
 
-  const addDataToCart = (e,user)=>{
-      e.preventDefault();
-      array.push(user);
-  
-    
+  const addDataToCart = (e, user) => {
+    e.preventDefault();
+    array.push(user);
+
+
     alert("Add to Cart  successfully  ")
   }
 
-  const gotoCart = (e)=>{
-      setcartData(array);
-      setShowCart(true);
+  const gotoCart = (e) => {
+    setcartData(array);
+    setShowCart(true);
   }
 
   useEffect(async () => {
@@ -56,69 +62,91 @@ const Displaymenus = () => {
   }, []);
 
 
+
+
+
+
+
+
+
   return (
-    <div className='container'>
-        <br></br>
-        <br></br>   <br></br>   <br></br>
+    <div className="">
+      <div style={{ textAlign: "center" }}>
+
+
         <div class="input-group">
-  <div className="col-md-9">
+          <div className="col-md-9">
 
-  <input type="search" class="form-control" style={{marginLeft:"25%" }} placeholder="Search by Food Name  " aria-label="Search"  onChange={event=>{setserachItem(event.target.value)}} 
-    aria-describedby="search-addon" />
-  </div>
-</div>
-<br></br><br></br>
-        <center>
-        <h1><b>Food Menus</b></h1>
-        <center>
-              <button className='btn btn-success' onClick={(e)=>gotoCart(e)}> View Cart </button>
-            </center>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <input type="search" class="form-control" style={{ marginLeft: "25%" }} placeholder="Search by Food Name  " aria-label="Search" onChange={event => { setserachItem(event.target.value) }}
+              aria-describedby="search-addon" />
+          </div>
+        </div>
 
-        </center>
-        
-        {ShowCart ? 
-            <AddToCart data={cartData}/> 
-          : 
-
-            <div class="col-11 col-md-6 col-lg-3 mx-0 mb-4" >
-
-            {users &&
-                    users.filter((users)=>{
-                      if(serachItem ==""){
-                            return users
-                      }else if(users.foodName.toLowerCase().includes(serachItem.toLowerCase())){
-                  
-                        return users
-            }   })
-                .map((user) => {
-                  return (
+        <div className="container">
+          <br></br>
+          <button className='btn btn-success' onClick={(e) => gotoCart(e)}> View Cart </button>
+          <br></br>      <br></br>
+          {ShowCart ?
+            <AddToCart data={cartData} />
+            :
+            <div className="">
+              <div className="">
 
 
-                <article class="card" style={{ flex: '0 1 20%', borderWidth: '2px', marginBottom: '10px' }}>
-                  <img src={user.images} alt='No Image Added...' style={{ width: '100%', height: 'auto' }} />
-                  <h4>{user.foodName}</h4>
-                  <p><b>LKR. {user.price}</b></p>
-                  
-                  <a className='btn btn-secondary'
-                      onClick={(e)=>{addDataToCart(e,user)}}
-                  ><b>Add to cart</b>
-                  {/* <i className="fa-solid fa-cart-circle-plus"></i> */}
-                  </a>
-                </article>
-                  );
-          })}
+                <div className="">
+
+                </div>
+                <div className="">
+
+                  <div className="row">
+                    <br></br><br></br>
+                    {users &&
+                      users.filter((users) => {
+                        if (serachItem == "") {
+                          return users
+                        } else if (users.foodName.toLowerCase().includes(serachItem.toLowerCase())) {
+
+                          return users
+                        }
+                      })
+                        .map((user) => {
+                          return (
 
 
-          
+                            <div className='col-11 col-md-6 col-lg-3 mx-0 mb-4'  key={1} >
+                            <div class="card p-0 overflow-hidden h-100 shadow">
+                                <img src={user.images}className='card-img-top img-fluid' />
+                                <div class="card-body text-center">
+                                    <h5 class="card-title">{user.foodName}</h5>
+                                    <p class="card-text">{user.RestaurantsType}</p>
+                                    <p class="card-text">{user.Description}</p>
+                                    <p class="card-text">LKR{user.price}</p>
+                                    <button class="btn btn-success"    onClick={(e) => { addDataToCart(e, user) }}>Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
 
+
+                      
+                          
+                          );
+                        })}
+
+                  </div>
+                </div>
+              </div>
             </div>
-        }
-
-
+          }
+        </div>
+      </div>
     </div>
-  )
+
+  );
+
 }
-
 export default Displaymenus;
-
 
