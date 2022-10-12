d820c4cdeed70d60b458fc0db9b1906eab4ff514
Minosh_Imantha <86770967+minosh00@users.noreply.github.com>
2022-10-12 20:20:19 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-app.use("/room", require("./routes/Room.Routes"));
-app.use("/employee", require("./routes/Employee.Routes"));
-app.use('/api', require('./routes/Comment.Routes'));
-app.use('/book', require('./routes/Booking.Routes'));
-app.use('/supplier', require('./routes/Supplier.route'));
-app.use("/menu", require("./Routes/food.routes"));
-app.use("/orders", require("./Routes/orders"));
